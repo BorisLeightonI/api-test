d587fc5e78aa6b4eef2a09922bdfdcbebde17b16
BorisLeightonI <borisleighton@gmail.com>
2022-10-18 15:03:28 -0500

    adding server.js

--- src/server0.js
+++ src/server0.js
@@ -1,20 +1,16 @@
 const express = require("express");
-require("dotenv").config();
-const morgan = require('morgan');
 const cors = require('cors')
-const { connect } = require("./db")
-// const applicationRoutes = require('./routes')
-
 
 const app = express();
 const port = process.env.PORT || 8080;
-connect();
+
 
 app.use(express.json())
-app.use(morgan('tiny'))
 app.use(cors())
 
-// applicationRoutes(app)
+app.get('/', (req, res)=>{
+  res.status(200).json({message:'ok', data:'ok'})
+})
 
 app.listen(port, () => {
   console.log(`✅ Server listening on port ${port}!`)
