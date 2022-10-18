8ab5f6d9e8acdf3ca0f3d4ae3b14c122e1788d24
BorisLeightonI <borisleighton@gmail.com>
2022-10-18 15:11:47 -0500

    comment dotenv and morgan

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-require("dotenv").config();
-const morgan = require('morgan');
+// require("dotenv").config();
+// const morgan = require('morgan');
 const cors = require('cors')
 const { connect } = require("./db")
 // const applicationRoutes = require('./routes')
